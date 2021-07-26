f94b11078b48d6d8e4aacffac53b92672e3122e2
ayoline <ayo@outlook.com.br>
2021-07-26 19:11:57 -0300

    adiciona buttons e metodo POST

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,9 +13,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _urlBase = 'https://jsonplaceholder.typicode.com';
+
   Future<List<Post>> _recuperarPostagens(http.Client client) async {
-    http.Response response = await client
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    http.Response response = await client.get(Uri.parse('$_urlBase/posts'));
     return compute(parsePostagens, response.body);
   }
 
@@ -25,44 +26,92 @@ class _HomeState extends State<Home> {
     return parsed.map<Post>((json) => Post.fromJson(json)).toList();
   }
 
+  _post() async {
+    var corpo = json.encode({
+      'userId': 120,
+      'id': null,
+      'title': 'Título',
+      'body': 'Corpo da postagem'
+    });
+
+    http.Response response = await http.post(Uri.parse('$_urlBase/posts'),
+        headers: {'Content-type': 'application/json; charset=UTF-8'},
+        body: corpo);
+
+    print('resposta: ${response.statusCode}');
+    print('resposta: ${response.body}');
+  }
+
+/*
+  _put() {}
+  _patch() {}
+  _delete() {}
+*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviço avançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(http.Client()),
-        builder: (context, snapshot) {
-          var retorno;
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: (CircularProgressIndicator()),
-              );
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                print('erro ao carregar');
-              } else {
-                print('lista carregou!');
-                retorno = ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      List<Post>? lista = snapshot.data;
-                      Post post = lista![index];
-                      return ListTile(
-                        contentPadding: const EdgeInsets.all(8.0),
-                        title: Text(post.title),
-                        subtitle: Text(post.body),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text('Salvar'),
+                  onPressed: _post,
+                ),
+                ElevatedButton(
+                  child: Text('Atualizar'),
+                  onPressed: _post,
+                ),
+                ElevatedButton(
+                  child: Text('Remover'),
+                  onPressed: _post,
+                ),
+              ],
+            ),
+
+            // serve para ocupar o espaço restante da tela com esse item
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPostagens(http.Client()),
+                builder: (context, snapshot) {
+                  var retorno;
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: (CircularProgressIndicator()),
                       );
-                    });
-              }
-              break;
-          }
-          return retorno;
-        },
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      if (snapshot.hasError) {
+                        print('erro ao carregar');
+                      } else {
+                        print('lista carregou!');
+                        retorno = ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              List<Post>? lista = snapshot.data;
+                              Post post = lista![index];
+                              return ListTile(
+                                contentPadding: const EdgeInsets.all(8.0),
+                                title: Text(post.title),
+                                subtitle: Text(post.body),
+                              );
+                            });
+                      }
+                      break;
+                  }
+                  return retorno;
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
